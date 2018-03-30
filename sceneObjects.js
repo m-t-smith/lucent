0acd4a6743d5754d747d7ccf7b325638c4034399
M Smith <al0101gorithm@gmail.com>
2018-03-30 17:16:21 -0400

    changed html file name to observe convention, made progress with sceneObject.js modularity/encapsulation

--- sceneObjects.js
+++ sceneObjects.js
@@ -1,28 +1,23 @@
  
- "use strict";
- 
- 
- 
+"use strict";
+
  var sceneObjectModule = (function () {
 	 
-	var mvMatrix = mat4.create();
-	var pMatrix = mat4.create();
-	/* var triangleVertexPositionBuffer;
-	var triangleVertexColorBuffer; */
 	
 	var objArray = [];
 	
-	createObject("triangle");
+	objArray.pBuff = gl.createBuffer();
+	objArray.cBuff = gl.createBuffer();
+	
+	for (var i = -2; i < 3; i++) {
+		createObject("triangle", {'place' : [i, 0.0, -10.0]});
+	}
+	
 	
 	function createObject(objClass, options) {
 		
 		let obj = {};
-		let pBuff = gl.createBuffer();
-		let cBuff = gl.createBuffer();
-		let a = {'key' : [0, 1, 2]};
-		console.log(a['key']);
-		console.log([2, 1, 0]);
-		
+	
 			switch (objClass) {
 				
 				case "triangle" :
@@ -31,9 +26,7 @@
 					obj.numVert = 3;
 					obj.colorSize = 4;
 					obj.numColor = 3;
-					obj.pBuff = pBuff;
-					obj.cBuff = cBuff;
-					
+				
 					let vertices = [
 						0.0,  1.0,  0.0,
 					   -1.0, -1.0,  0.0,
@@ -42,7 +35,8 @@
 						
 					obj.v = vertices;
 					
-					if (!options){
+					//check if there are parameters for size and color, if not, use default values
+					if (!options){  
 						obj.place = [0.0, 0.0, -7.0];
 						
 						let colors = [
@@ -55,12 +49,22 @@
 						
 					} else {
 					
-						if (options['place']) {
-							obj.place = options['place'];
+						if (options.place) {
+							obj.place = options.place;
 						}	
-						if (options['color']) {
-							obj.c = options['color'];
-						} 
+						if (options.color) {
+							obj.c = options.color;
+							
+						} else {
+							
+							let colors = [
+							1.0, 0.0, 0.0, 1.0,
+							0.0, 1.0, 0.0, 1.0,
+							0.0, 0.0, 1.0, 1.0
+							];
+							
+							obj.c = colors;
+						}
 					}
 					
 					objArray.push(obj);
@@ -70,46 +74,57 @@
 			}
 	}
 
-function setMatrixUniforms() {
+function setMatrixUniforms(pMatrix, mvMatrix) {
 	gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
 	gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
 	}
 
 
 function initBuffers() {
-		var currObj = objArray[0];
 	
-		gl.bindBuffer(gl.ARRAY_BUFFER, currObj.pBuff);
-       
-		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(currObj.v), gl.STATIC_DRAW);
-    
-		gl.bindBuffer(gl.ARRAY_BUFFER, currObj.cBuff);
+	let currObj = objArray[0];
+	gl.bindBuffer(gl.ARRAY_BUFFER, objArray.pBuff);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(currObj.v), gl.STATIC_DRAW);
+		
+	gl.bindBuffer(gl.ARRAY_BUFFER, objArray.cBuff);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(currObj.c), gl.STATIC_DRAW);
+	//Need to develope some way to check whether objects are different
+	//which will effect the buffers we need to use
 
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(currObj.c), gl.STATIC_DRAW);
-       
 }
 
 function drawScene() {
-		let currObj = objArray[0];
 		
-        gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+		let mvMatrix = mat4.create();
+		let pMatrix = mat4.create();
+	
+	
+	
+		gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+		gl.clear(gl.COLOR_BUFFER_BIT || gl.DEPTH_BUFFER_BIT);
 
-        mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
+		mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
 
-        mat4.identity(mvMatrix);
-
-        mat4.translate(mvMatrix, currObj.place);
         
-		gl.bindBuffer(gl.ARRAY_BUFFER, currObj.pBuff);
-		gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, currObj.vertSize, gl.FLOAT, false, 0, 0);
-      
-		gl.bindBuffer(gl.ARRAY_BUFFER, currObj.cBuff);
-		gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, currObj.colorSize, gl.FLOAT, false, 0, 0);
-        
-		setMatrixUniforms();
-        
-		gl.drawArrays(gl.TRIANGLES, 0, currObj.numVert);
+		
+		for (let i = 0; i < objArray.length; i++){
+			
+			
+			let currObj = objArray[i];
+			
+			mat4.identity(mvMatrix);
+			mat4.translate(mvMatrix, currObj.place);
+			
+			gl.bindBuffer(gl.ARRAY_BUFFER, objArray.pBuff);
+			gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, currObj.vertSize, gl.FLOAT, false, 0, 0);
+		  
+			gl.bindBuffer(gl.ARRAY_BUFFER, objArray.cBuff);
+			gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, currObj.colorSize, gl.FLOAT, false, 0, 0);
+			
+			setMatrixUniforms(pMatrix, mvMatrix);
+			
+			gl.drawArrays(gl.TRIANGLES, 0, currObj.numVert);
+		}
 		
 }
 
