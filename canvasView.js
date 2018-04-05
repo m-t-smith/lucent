a92eaccfd7eb1c81b62997240960378dbe6cdac7
Matt Smitherman <mtsmithe@uncg.edu>
2018-04-05 12:34:02 -0400

    had to commit to merge with remote

---
+++ canvasView.js
@@ -0,0 +1,34 @@
+//canvas view
+
+Vue.component("canvas-view", {
+	template: "#canvas-view-template",
+	props: ["objArray"],
+	data: function () {
+		return {
+			isVisible: true,
+			message: "",
+			height: 800,
+			width: 800
+		}
+	},
+	computed: {
+		update: function() {
+			
+		}
+	}, 
+		methods: {
+			show: function() {
+				this.isVisible = true;
+			},
+			hide: function() {
+				this.isVisible = false;
+			},
+			toggle: function() {
+				this.isVisible = !this.isVisible;
+			}
+		},
+		
+		mounted: function() {
+			this.message = "I am a canvas";
+		}
+})
