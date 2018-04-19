55d1aa1da758520ba29c2e3c6be33ec9125da8db
Matthew T Smitherman <MTSMITHE@campus.uncg.edu>
2018-04-18 21:01:01 -0400

    prototype test working feels good man

--- test/protoTest.js
+++ test/protoTest.js
@@ -1,4 +1,4 @@
-function BaseObject( vertSize, numVert, colorSize, numColor, vertPosArr, vertColorArr){
+function BaseObject(vertSize, numVert, colorSize, numColor, vertPosArr, vertColorArr){
 	this.vs = vertSize;
 	this.nv = numVert;
 	this.cs = colorSize;
@@ -7,4 +7,30 @@ function BaseObject( vertSize, numVert, colorSize, numColor, vertPosArr, vertCol
 	this.vca = vertColorArr;
 }
 
-BaseObject.prototype.
+BaseObject.prototype.setMatrixUniforms = function(pM, mvM) {
+	
+	console.log("Uniform matricies set");
+	
+	gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pM);
+	gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvM);
+};
+
+BaseObject.prototype.initBuffers = function(pbuff, cbuff, pArr, cArr) {
+	
+	console.log("buffers are bound");
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, pbuff);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(pArr), gl.STATIC_DRAW);
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, cbuff);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cArr), gl.STATIC_DRAW);
+};
+
+BaseObject.prototype.bindBuffSetAttrib = function(pbuff, cbuff) {
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, pbuff);
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, this.vs, gl.FLOAT, false, 0, 0);
+		  
+	gl.bindBuffer(gl.ARRAY_BUFFER, cbuff);
+	gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, this.cs, gl.FLOAT, false, 0, 0);
+};
